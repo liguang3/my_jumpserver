113d85ba3e789bacbc62f2a4b642421d84804b9e
liguang <13623715177@163.com>
2017-09-18 09:59:20 +0800

    my_jump

---
+++ my_jump/conf/settings.py
@@ -0,0 +1,24 @@
+#_*_coding:utf-8_*_
+
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+DB_CONN ="mysql+pymysql://mysql:12345678@l13.200.60.105:3306/my_jump?charset=utf8"
+
+'''
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysqldb',
+        'NAME': 'my_jump',
+        'HOST': '',
+        'PORT':3306,
+        'USER':'mysql',
+        'PASSWORD': '123456'
+    }
+}
+'''
+
